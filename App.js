038c10b3c6502bfbb76ae4b0523ddd1ab78936fc
yoncacoskun <yonca.coskun@hotmail.com.tr>
2018-04-13 13:12:35 +0300

    Initial Commit

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-import { AppRegistry ,ScrollView,View,Text,Animated, Dimensions, StyleSheet} from "react-native";
 import StartScreen from './src/components/StartScreen';
 import { StackNavigator } from "react-navigation";
 import QuesScreen from './src/components/QuesScreen';
 import FinishScreen from './src/components/FinishScreen';
-import CardItem from './src/components/CardItem';
-import QuestionDetail from "./src/components/QuestionDetail";
+
 
 const reactNavigationSample = props => {
   return <StartScreen navigation={props.navigation} />;
